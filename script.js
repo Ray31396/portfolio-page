56db07a7234e5084c6ea8735aaa6c5ae56c7bbbb
rayvongregory <rayray31396@gmail.com>
2021-07-13 19:28:04 -0400

    Edited the menu options and added some js for pointerover and pointerout events

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//! This is the hamburger menu stuff//
+
 // const nav = document.querySelector(".nav");
 // const navHamburger = document.querySelector(".nav___hamburger");
 // const hamburgerLines = document.getElementsByClassName("nav___hamburger-line");
@@ -12,3 +14,14 @@
 //     });
 //   }
 // });
+
+const smileIcon = document.querySelector(".fa-smile-beam");
+const smileA = document.querySelector(".smile");
+
+smileA.addEventListener("pointerover", () => {
+  smileIcon.classList.replace("fa-smile-beam", "fa-grimace");
+});
+
+smileA.addEventListener("pointerout", () => {
+  smileIcon.classList.replace("fa-grimace", "fa-smile-beam");
+});
