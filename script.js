eb59f102b871d5bea5aec7ce6892c79987b3fed4
rayvongregory <rayray31396@gmail.com>
2021-07-13 17:52:33 -0400

    Commeneted out hamburger nav html and css, trying the tabs-at-bottom approach

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const nav = document.querySelector(".nav");
-const navHamburger = document.querySelector(".nav___hamburger");
-const hamburgerLines = document.getElementsByClassName("nav___hamburger-line");
-const navItems = document.querySelector(".nav___items");
+// const nav = document.querySelector(".nav");
+// const navHamburger = document.querySelector(".nav___hamburger");
+// const hamburgerLines = document.getElementsByClassName("nav___hamburger-line");
+// const navItems = document.querySelector(".nav___items");
 
-document.addEventListener("click", (e) => {
-  if (e.target === nav) {
-    nav.ariaPressed = nav.ariaPressed === "false" ? "true" : "false";
-    navItems.classList.toggle("hideItems");
-    Array.from(hamburgerLines).forEach((line, x) => {
-      line.classList.toggle(`line-${x + 1}`);
-    });
-  }
-});
+// document.addEventListener("click", (e) => {
+//   if (e.target === nav) {
+//     nav.ariaPressed = nav.ariaPressed === "false" ? "true" : "false";
+//     navItems.classList.toggle("hideItems");
+//     Array.from(hamburgerLines).forEach((line, x) => {
+//       line.classList.toggle(`line-${x + 1}`);
+//     });
+//   }
+// });
