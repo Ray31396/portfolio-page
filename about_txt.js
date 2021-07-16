5927c8cd9ab00c2f53031e26cdda07a8cbb15222
rayvongregory <rayray31396@gmail.com>
2021-07-15 20:04:42 -0400

    Played around with about section text formatting/margins

--- about_txt.js
+++ about_txt.js
@@ -1,4 +1,5 @@
 export const fullAbout =
-  "<h3>This is the full about section.</h3> <p>This is a paragraph.</p>";
+  "<h3>This is the full about section.</h3> <p>This is a paragraph. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias eos fugit quod, nulla architecto ab quidem facere optio deleniti impedit quis a excepturi sint velit ipsam repudiandae harum nisi. Animi deserunt fuga sapiente. Repellendus, voluptas! Aliquid, eius quaerat quae modi inventore tempore debitis possimus laboriosam deserunt laudantium sed similique quod.</p>";
 
-export const tldrAbout = "This is the tldr about section.";
+export const tldrAbout =
+  "<h3>This is the tldr about section.</h3> <p>Just another former teacher trying to land their first tech job. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias eos fugit quod, nulla architecto ab quidem facere optio deleniti impedit quis a excepturi sint velit ipsam repudiandae harum nisi. Animi deserunt fuga sapiente. Repellendus, voluptas! Aliquid, eius quaerat quae modi inventore tempore debitis possimus laboriosam deserunt laudantium sed similique quod. </p>";
--- script.js
+++ script.js
@@ -3,7 +3,14 @@ const smileIcon = document.querySelector(".fa-smile-beam");
 const smileA = document.querySelector(".smile");
 const togLabel = document.querySelector(".switch");
 const togBtn = document.querySelector("#togBtn");
+const aboutBody = document.getElementsByClassName("card___body")[0];
+const aboutHead = document
+  .querySelector(".card___body-top-text")
+  .querySelectorAll(":scope > *");
 const aboutText = document.getElementsByClassName("card___body-bottom")[0];
+const aboutImg = document.querySelector(".card___image");
+
+console.log(aboutHead);
 
 window.onload = (event) => {
   aboutText.innerHTML = fullAbout;
@@ -21,13 +28,23 @@ togLabel.addEventListener(
   "pointerup",
   (e) => {
     e.preventDefault();
-    aboutText.classList.remove("anim");
-    void aboutText.offsetWidth;
-    aboutText.classList.add("anim");
+    aboutBody.classList.remove("anim");
+    void aboutBody.offsetWidth;
+    aboutBody.classList.add("anim");
     if (togBtn.checked) {
       aboutText.innerHTML = fullAbout;
+      aboutText.style.margin = "0 auto";
+      aboutImg.classList.remove("hide");
+      aboutHead.forEach((element) => {
+        element.style.textAlign = "center";
+      });
     } else {
       aboutText.innerHTML = tldrAbout;
+      aboutText.style.margin = "initial";
+      aboutImg.classList.add("hide");
+      aboutHead.forEach((element) => {
+        element.style.textAlign = "left";
+      });
     }
   },
   false
