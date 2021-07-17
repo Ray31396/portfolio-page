ddc3544b7d7db9da204b4576aaf6e703fa3f5674
rayvongregory <rayray31396@gmail.com>
2021-07-17 04:04:11 -0400

    Got sad when I noticed the last attempt to fix safari issue didn't work so I played around with font sizes and margin for a while

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 import { fullAbout, tldrAbout } from "./about_txt.js";
 const smileIcon = document.querySelector(".fa-smile-beam");
 const smileA = document.querySelector(".smile");
+const togLabel = document.querySelector(".switch");
+const togSlider = document.querySelector(".slider");
 const togBtn = document.querySelector("#togBtn");
 const aboutBody = document.getElementsByClassName("card___body")[0];
-const aboutHead = document
-  .querySelector(".card___body-top-text")
-  .querySelectorAll(":scope > *");
+const aboutHead = document.querySelector(".card___body-top-text");
 const aboutText = document.getElementsByClassName("card___body-bottom")[0];
 const aboutImg = document.querySelector(".card___image");
+
+console.log(aboutHead);
 
 window.onload = (event) => {
   aboutText.innerHTML = fullAbout;
@@ -21,31 +23,27 @@ smileA.addEventListener("pointerout", () => {
   smileIcon.classList.replace("fa-grimace", "fa-smile-beam");
 });
 
-document.addEventListener(
+togLabel.addEventListener(
   "pointerup",
-  () => {
+  (e) => {
+    e.preventDefault();
+    aboutBody.classList.remove("anim");
+    void aboutBody.offsetWidth;
+    aboutBody.classList.add("anim");
     if (togBtn.checked) {
       aboutText.innerHTML = fullAbout;
       aboutText.style.margin = "0 auto";
-      aboutBody.style.marginLeft = "0";
       aboutImg.classList.remove("hide");
-      aboutHead.forEach((element) => {
+      aboutHead.querySelectorAll(":scope > *").forEach((element) => {
         element.style.textAlign = "center";
       });
-      aboutBody.classList.remove("anim");
-      void aboutBody.offsetWidth;
-      aboutBody.classList.add("anim");
     } else {
       aboutText.innerHTML = tldrAbout;
-      aboutText.style.margin = "initial";
-      aboutBody.style.marginLeft = "0.5em";
+      aboutText.style.margin = "unset";
       aboutImg.classList.add("hide");
-      aboutHead.forEach((element) => {
+      aboutHead.querySelectorAll(":scope > *").forEach((element) => {
         element.style.textAlign = "left";
       });
-      aboutBody.classList.remove("anim");
-      void aboutBody.offsetWidth;
-      aboutBody.classList.add("anim");
     }
   },
   false
