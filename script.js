8bf38576c4c453060c34694ca35a817547c750c5
rayvongregory <rayray31396@gmail.com>
2022-01-22 05:17:20 -0500

    Finally working on this again. Got rid of some unnecessary html tags and js, added some media queries and rewrote my cover letter

--- script.js
+++ script.js
@@ -1,55 +1,50 @@
-import { fullAbout, tldrAbout } from "./about_txt.js";
-const smileIcon = document.querySelector(".fa-smile-beam");
-const smileA = document.querySelector(".smile");
-const aboutHead = document.querySelector(".about-card-head");
-const aboutBody = document.getElementsByClassName("card___body")[0];
-const aboutText = document.getElementsByClassName("card___body-bottom")[0];
-const aboutImg = document.querySelector(".card___image");
-const slider = document.querySelector(".slider");
-const sliderBall = document.querySelector(".slider___ball");
-const sliderText = document.querySelector(".slider___text");
+const aboutHead = document.querySelector(".card___body-top"),
+  aboutBody = document.getElementsByClassName("card___body")[0],
+  fullTxt = document.querySelector(".full"),
+  tldrTxt = document.querySelector(".tldr"),
+  aboutImg = document.querySelector(".card___image"),
+  slider = document.querySelector(".slider"),
+  sliderBall = document.querySelector(".slider___ball"),
+  sliderText = document.querySelector(".slider___text")
 
-window.onload = (event) => {
-  aboutText.innerHTML = fullAbout;
-};
-
-smileA.addEventListener("pointerover", () => {
-  smileIcon.classList.replace("fa-smile-beam", "fa-grimace");
-});
-
-smileA.addEventListener("pointerout", () => {
-  smileIcon.classList.replace("fa-grimace", "fa-smile-beam");
-});
+Array.from(document.querySelectorAll(".pr-nums button")).forEach((btn) => {
+  btn.addEventListener("pointerup", (e) => {
+    const { id } = e.target
+    document
+      .getElementById(id.split("-")[0])
+      .scrollIntoView({ block: "nearest", inline: "nearest" })
+  })
+})
 
 slider.addEventListener(
   "pointerup",
   (e) => {
-    e.preventDefault();
-    aboutBody.classList.remove("anim");
-    void aboutBody.offsetWidth;
-    aboutBody.classList.add("anim");
-    sliderBall.classList.toggle("slide");
-    sliderText.classList.toggle("to-right");
-    sliderText.classList.remove("anim");
-    void aboutBody.offsetWidth;
-    sliderText.classList.add("anim");
+    e.preventDefault()
+    aboutBody.classList.remove("anim")
+    void aboutBody.offsetWidth
+    aboutBody.classList.add("anim")
+    sliderBall.classList.toggle("slide")
+    sliderText.classList.toggle("to-right")
+    sliderText.classList.remove("anim")
+    void aboutBody.offsetWidth
+    sliderText.classList.add("anim")
     if (!sliderBall.classList.contains("slide")) {
-      sliderText.innerHTML = "full";
-      aboutText.innerHTML = fullAbout;
-      aboutText.style.setProperty("margin", "0 auto");
-      aboutImg.classList.remove("hide");
-      aboutHead.querySelectorAll(":scope > *").forEach((element) => {
-        element.style.setProperty("text-align", "center");
-      });
+      sliderText.innerText = "full"
+      tldrTxt.classList.add("hide")
+      fullTxt.classList.remove("hide")
+      aboutImg.classList.remove("hide")
+      Array.from(aboutHead.children).forEach((element) => {
+        element.style.setProperty("text-align", "center")
+      })
     } else {
-      sliderText.innerHTML = "tldr";
-      aboutText.innerHTML = tldrAbout;
-      aboutText.style.setProperty("margin", "unset");
-      aboutImg.classList.add("hide");
-      aboutHead.querySelectorAll(":scope > *").forEach((element) => {
-        element.style.setProperty("text-align", "left");
-      });
+      sliderText.innerText = "tldr"
+      tldrTxt.classList.remove("hide")
+      fullTxt.classList.add("hide")
+      aboutImg.classList.add("hide")
+      Array.from(aboutHead.children).forEach((element) => {
+        element.style.setProperty("text-align", "left")
+      })
     }
   },
   false
-);
+)
